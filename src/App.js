33d6f0f90306802935da2647400a550bc4b163a2
teni-dev <66254545+teni-dev@users.noreply.github.com>
2022-10-08 22:14:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Support from './components/Support';
+import About from './components/About';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Allinone from './components/Allinone';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Support />
+    <Allinone />
+    <Pricing />
+    <Footer />
+    </>
+     
+  )
 }
 
 export default App;
